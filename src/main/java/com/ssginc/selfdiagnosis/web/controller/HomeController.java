10ec3f6d5569ef8dde0e4612216eeb84149e4640
juho <yjuho@shinsegae.com>
2020-09-17 17:19:34 +0900

    사용자 화면 1차 개발

--- src/main/java/com/ssginc/selfdiagnosis/web/controller/HomeController.java
+++ src/main/java/com/ssginc/selfdiagnosis/web/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.ssginc.selfdiagnosis.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class HomeController {
@@ -11,5 +12,26 @@ public class HomeController {
     public String manageGroup(){
         System.out.println("group test 1");
         return "group_manage";
+    }
+
+    @GetMapping(value = "/userChecklist")
+    public ModelAndView userCheckList(){
+        ModelAndView mav = new ModelAndView("user_checklist");
+
+        return mav;
+    }
+
+    @GetMapping(value = "/userMain")
+    public ModelAndView userMain(){
+        ModelAndView mav = new ModelAndView("user_main");
+
+        return mav;
+    }
+
+    @GetMapping(value = "/userResult")
+    public ModelAndView userResult(){
+        ModelAndView mav = new ModelAndView("user_result");
+
+        return mav;
     }
 }
